53d885d14b441ea71be08105f67d585023749ba3
Jennifer de Souza <jdszza.devs@gmail.com>
2024-09-26 14:56:32 -0300

    password

--- js/script.js
+++ js/script.js
@@ -6,14 +6,6 @@ function showScreen(screenId) {
     document.getElementById(screenId).classList.remove('hidden');
 }
 
-document.querySelector('.show-password').addEventListener('click', function () {
-    const senhaInput = document.getElementById('senha');
-    if (senhaInput.type === 'password') {
-        senhaInput.type = 'text';
-    } else {
-        senhaInput.type = 'password';
-    }
-});
 
 // Inicializa a primeira tela como Splash Screen
 showScreen('splash-screen');
